f247236df45717395980cb64f7430d4ef625403b
David <David@DESKTOP-KAPVBTV>
2024-08-28 17:30:46 -0400

    Creacion clases

--- SIA/src/main/java/com/mycompany/sia/SIA.java
+++ SIA/src/main/java/com/mycompany/sia/SIA.java
@@ -11,52 +11,6 @@ package com.mycompany.sia;
  */
 import java.util.*;
 
-class Pasajero{
-    private String nombre;
-    private int edad;
-    private String correo;
-    
-    public void setNombre(String nombre){
-        this.nombre = nombre;
-    }
-    public void setEdad(int edad){
-        this.edad = edad;
-    }
-    public void setCorreo(String correo){
-        this.correo = correo;
-    }
-    public void setCorreo(){
-        correo = "No hay correo";
-    }
-    
-    public void Pasajero(String nombre, int edad, String correo){
-        setNombre(nombre);
-        setEdad(edad);
-        setCorreo(correo);
-    }
-    public void Pasajero(String nombre, int edad){
-        setNombre(nombre);
-        setEdad(edad);
-        setCorreo();
-    }
-    
-    public String getNombre(){
-        return nombre;
-    }
-    public int getEdad(){
-        return edad;
-    }
-    public String getCorreo(){
-        return correo;
-    }
-    
-    public void MostrarPasajero(){
-        System.out.println("Nombre: "+ getNombre());
-        System.out.println("Edad: "+ getEdad());
-        System.out.println("Correo: "+ getCorreo());
-    }
-}
-
 class Asiento {
     private String numeroAsiento;
     private boolean ocupado;
