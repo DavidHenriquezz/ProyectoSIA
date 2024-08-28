2b094a9255b03225c778ba080ed44556bd604a70
David <David@DESKTOP-KAPVBTV>
2024-08-27 21:03:08 -0400

    Creación clase pasajero y funciones set

--- SIA/src/main/java/com/mycompany/sia/SIA.java
+++ SIA/src/main/java/com/mycompany/sia/SIA.java
@@ -9,7 +9,19 @@ package com.mycompany.sia;
  * @author David
  */
 class Pasajero{
+    private String nombre;
+    private int edad;
+    private String correo;
     
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+    public void setCorreo(String correo){
+        this.correo = correo;
+    }
 }
 public class SIA {
 
